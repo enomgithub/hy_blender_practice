05b7364d4b8ce88c95c3aba53f5763615698a83a
enom <satoshi.enom@gmail.com>
2018-05-25 23:16:35 +0900

    Add sphere animation (linear)

---
+++ run_sphere_mov.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+import hy
+
+import sphere_mov
+
+
+def main():
+    sphere_mov.main()
+    return 0
+
+
+if __name__ == "__main__":
+    main()
